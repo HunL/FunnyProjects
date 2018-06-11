61b1659429a82c691f34184d791ff9373eaddf18
lijiehui <ljiehui0826@gmail.com>
2018-06-11 20:55:39 +0800

    smartContract succeed

--- homework/smartContract/homework.js
+++ homework/smartContract/homework.js
@@ -21,19 +21,9 @@ HomeWorkItem.prototype = {
 	}
 };
 
-// var HomeWorkKeyArray = function () {
-//     LocalContractStorage.defineMapProperty(this, "repo", {
-//         stringify: function (obj) {
-//             return obj.toString();
-//         },
-//         parse: function (text) {
-//             return new HomeWorkItem(text);
-//         }
-//     });
-// };
-
 var SuperHomeWork = function () {
-    LocalContractStorage.defineMapProperty(this, "repo", {
+    LocalContractStorage.defineMapProperty(this, "dataMap", {
+    // LocalContractStorage.defineMapProperty(this, "repo", {
         stringify: function (obj) {
             return obj.toString();
         },
@@ -42,21 +32,25 @@ var SuperHomeWork = function () {
         }
     });
 
-    LocalContractStorage.defineMapProperty(this, "repoArray", {
-        stringify: function (obj) {
-            // 存储时直接转为字符串
-            return obj.toString();
-        },
-        parse: function (text) {
-            // 读取时获得HomeWorkItem对象
-            return new HomeWorkItem(text);
-        }
-    });
+    // LocalContractStorage.defineMapProperty(this, "repoArray", {
+    //     stringify: function (obj) {
+    //         // 存储时直接转为字符串
+    //         return obj.toString();
+    //     },
+    //     parse: function (text) {
+    //         // 读取时获得HomeWorkItem对象
+    //         return new HomeWorkItem(text);
+    //     }
+    // });
+    LocalContractStorage.defineMapProperty(this, "arrayMap");
+    // LocalContractStorage.defineMapProperty(this, "dataMap");
+    LocalContractStorage.defineProperty(this, "size");
 
 };
 
 SuperHomeWork.prototype = {
     init: function () {
+        this.size = 0;
     },
 
     save: function (key, value) {
@@ -77,23 +71,57 @@ SuperHomeWork.prototype = {
         homeworkItem.value = value;
         homeworkItem.time = Date.Now();
 
-        this.repo.put(key, homeworkItem);
+        var index = this.size;
+        this.arrayMap.set(index, key);
+        this.dataMap.set(key, homeworkItem);
+        // this.repo.put(key, homeworkItem);
 
         // this.HomeWorkKeyArray.push(homeworkItem);
         // this.repo.HomeWorkKeyArray.push(homeworkItem);
-        this.repoArray.push(key, homeworkItem);
+        // this.repoArray.push(key, homeworkItem);
+        this.size += 1;
     },
 
-    get: function () {
-        console.log("here get");
+    // get: function () {
+    //     console.log("here get");
+    //     this.repoArray.sort(
+    //         function(v1, v2){
+    //             return v1.time-v2.time;
+    //         });
+    //     return this.repoArray.splice(0,4);
+    // }
+    get: function (key) {
+        // return this.dataMap.get(key);
+        return "111";
+    },
 
-        // this.repo.HomeWorkKeyArray.sort(
-        this.repoArray.sort(
-            function(v1, v2){
-                return v1.time-v2.time;
-            });
-        // return this.HomeWorkKeyArray.splice(0,4);
-        return this.repoArray.splice(0,4);
+    len: function () {
+        // return this.size;
+        return "222";
+    },
+
+    fiveData: function (offset) {
+    // fiveData: function (limit, offset) {
+        // limit = parseInt(limit);
+        limit = parseInt(5);
+        offset = parseInt(offset);
+
+        if (offset > this.size) {
+            throw new Error("offset is not valid");
+        }
+
+        var num = offset + limit;
+        if (num > this.size) {
+            num = this.size;
+        }
+
+        var result = "";
+        for (var i = offset; i < num; i++) {
+            var key = this.arrayMap.get(i);
+            var obj = this.dataMap.get(key);
+            result += "index: " + i + " key: " + key + " value: " + obj + "_";
+        }
+        return result;
     }
 };
 
